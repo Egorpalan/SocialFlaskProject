05f67461724bec70eb1df695a88a1d14c243befc
Egorpalan <Egorpalan@yandex.ru>
2023-10-06 21:29:35 +0400

    make user/get requests and make email validation

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+import re
+
+
 class User:
     def __init__(self, id, first_name, last_name, email, total_reactions, posts=[]):
         self.id = id
@@ -6,3 +9,11 @@ class User:
         self.email = email
         self.total_reactions = total_reactions
         self.posts = posts
+
+    @staticmethod
+    def is_valid_email(email):
+        patterns = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+        if re.match(patterns, email):
+            return True
+        else:
+            return False
--- app/views.py
+++ app/views.py
@@ -17,9 +17,32 @@ def user_create():
     last_name = data.get("last_name")
     email = data.get("email")
     total_reactions = data.get("total_reactions")
-    # todo: validate email
+    if not models.User.is_valid_email(email):
+        return Response(status=HTTPStatus.BAD_REQUEST)
     user = models.User(id, first_name, last_name, email, total_reactions)
     USERS.append(user)
+    response = Response(
+        json.dumps(
+            {
+                "id": user.id,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "email": user.email,
+                "total_reactions": user.total_reactions,
+                "posts": user.posts,
+            }
+        ),
+        HTTPStatus.OK,
+        mimetype="application/json",
+    )
+    return response
+
+
+@app.get("/users/<int:user_id>")
+def get_user(user_id):
+    if user_id < 0 or user_id >= len(USERS):
+        return Response(status=HTTPStatus.NOT_FOUND)
+    user = USERS[user_id]
     response = Response(
         json.dumps(
             {
